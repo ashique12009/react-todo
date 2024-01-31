2bc8c0245833bb8b6d0f69ced5936a600c2c38b1
Ashique <ashique12009@gmail.com>
2024-01-31 13:33:50 +0200

    update operation done

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import TodoList from './TodoList';
-
 import toast, { Toaster } from 'react-hot-toast';
 
 const TodoForm = () => {
     const [title, setTitle] = useState("");
+    const [itemId, setItemId] = useState(0);
     const [loader, setLoader] = useState(false);
     const [list, setList] = useState([]);
+    const [addOrUpdate, setAddOrUpdate] = useState('add');
 
     const onTitleClick = (itemTitle, itemId) => {
         console.log('ITEM', itemTitle, itemId);
+        setTitle(itemTitle);
+        setItemId(itemId);
+        setAddOrUpdate('update');
     }
 
     const fetchData = async (operation = '') => {
@@ -64,25 +68,50 @@ const TodoForm = () => {
         }
 
         try {
-            // Make POST request
-            const postURL = `http://localhost/classic-php-todo-rest-api/index.php`;
-            const postHeaders = new Headers();
-            postHeaders.append('token', 'secret_token');
+            setLoader(true);
+            if (addOrUpdate == 'update') {
+                // Make PUT request
+                const putURL = `http://localhost/classic-php-todo-rest-api/index.php`;
+                const putHeaders = new Headers();
+                putHeaders.append('token', 'secret_token');
 
-            let payload = JSON.stringify({ 'title': title });
+                let payload = JSON.stringify({ 'title': title, 'id': itemId });
 
-            const postOptions = {
-                method: 'POST',
-                headers: postHeaders,
-                redirect: 'follow',
-                body: payload
-            };
+                const putOptions = {
+                    method: 'PUT',
+                    headers: putHeaders,
+                    redirect: 'follow',
+                    body: payload
+                };
 
-            const postResponse = await fetch(postURL, postOptions);
+                const putResponse = await fetch(putURL, putOptions);
 
-            if (!postResponse.ok) {
-                toast.error(`HTTP error! Status: ${postResponse.status}`);
-                throw new Error(`HTTP error! Status: ${postResponse.status}`);
+                if (!putResponse.ok) {
+                    toast.error(`HTTP error! Status: ${putResponse.status}`);
+                    throw new Error(`HTTP error! Status: ${putResponse.status}`);
+                }
+            }
+            else {
+                // Make POST request
+                const postURL = `http://localhost/classic-php-todo-rest-api/index.php`;
+                const postHeaders = new Headers();
+                postHeaders.append('token', 'secret_token');
+
+                let payload = JSON.stringify({ 'title': title });
+
+                const postOptions = {
+                    method: 'POST',
+                    headers: postHeaders,
+                    redirect: 'follow',
+                    body: payload
+                };
+
+                const postResponse = await fetch(postURL, postOptions);
+
+                if (!postResponse.ok) {
+                    toast.error(`HTTP error! Status: ${postResponse.status}`);
+                    throw new Error(`HTTP error! Status: ${postResponse.status}`);
+                }
             }
 
             setLoader(false);
