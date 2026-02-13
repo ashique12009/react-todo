3ed97e85ca775ab0713f5b5309a227e323933e9b
Ashique <ashique12009@gmail.com>
2026-02-13 07:30:47 +0200

    css fix

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const List = ({ todoList, onDelete }) => {
-  console.log("Todo list in List component:", todoList);
   return (
     <div className="todo-list" id="todoList">
       {/* List of todos will be rendered here */}
@@ -10,11 +9,18 @@ const List = ({ todoList, onDelete }) => {
           <div
             className={`status-bar ${todo.status.toLowerCase().replace(" ", "-")}`}
           ></div>
+          
           <div className="todo-content">
             <div className="todo-title text-left">{todo.title}</div>
-            <div className="todo-deadline">Deadline: {todo.deadline}</div>
+            <div className="todo-deadline text-left">Deadline: {todo.deadline}</div>
           </div>
-          <div className="todo-delete" onClick={() => onDelete(todo.id)}>X</div>
+
+          <div className="todo-actions">
+            <div className="todo-edit">Edit</div>
+            <div className="todo-delete" onClick={() => onDelete(todo.id)}>
+              X
+            </div>
+          </div>
         </div>
       ))}
     </div>
