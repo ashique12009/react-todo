8a1800a3e3fe3b1eba3f5f654a70f8f61ebc890d
Ashique <ashique12009@gmail.com>
2026-02-14 08:02:39 +0200

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Modal from "./components/Modal";
 import List from "./components/List";
 import Legend from "./components/Legend";
+import useTodos from "./hooks/useTodos";
 
 function App() {
-  const API_URL = "http://localhost:3001/todos";
+  const { todos, addTodo, deleteTodo, updateTodo } = useTodos();
   const [openModal, setOpenModal] = useState(false);
-  const [todos, setTodos] = useState([]);
 
   const [selectedTodo, setSelectedTodo] = useState(null);
-
-  // Get all todos
-  const fetchTodos = async () => {
-    try {
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      setTodos(data);
-    } catch (error) {
-      console.error("Error fetching todos:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  // Add a new todo
-  const addTodo = async (newTodo) => {
-    try {
-      const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newTodo),
-      });
-
-      // Refresh the list after deleting a todo
-      fetchTodos();
-    } catch (error) {
-      console.error("Error adding todo:", error);
-    }
-  };
-
-  // Delete a todo
-  const deleteTodo = async (id) => {
-    try {
-      const response = await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
-      });
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-    }
-
-    // Refresh the list after deleting a todo
-    fetchTodos();
-  };
-
-  // Update a todo
-  const updateTodo = async (updatedTodo) => {
-    try {
-      await fetch(`${API_URL}/${updatedTodo.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedTodo),
-      });
-
-      fetchTodos();
-    } catch (error) {
-      console.error("Error updating todo:", error);
-    }
-  };
 
   return (
     <>
@@ -95,8 +31,8 @@ function App() {
         <Modal
           open={openModal}
           setOpen={setOpenModal}
-          onAdd={addTodo} 
-          onUpdate={updateTodo} 
+          onAdd={addTodo}
+          onUpdate={updateTodo}
           selectedTodo={selectedTodo}
         />
 
